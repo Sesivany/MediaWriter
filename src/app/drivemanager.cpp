cc5cc65a9c99ffaf1a3c75e86c32d3a9ca3d9316
Jiri Eischmann <jiri@eischmann.cz>
2025-07-23 11:50:22 +0200

    Experimental implementation of the suspend inhibitor

--- src/app/drivemanager.cpp
+++ src/app/drivemanager.cpp
@@ -34,6 +34,8 @@
 #include "fakedrivemanager.h"
 
 #include <QtQml>
+#include "suspendinhibitor.h"
+#include "suspendmanager.h"
 
 DriveManager *DriveManager::_self = nullptr;
 
@@ -294,10 +296,41 @@ void Drive::setDelayedWrite(const bool &o)
     if (m_delayedWrite != o) {
         m_delayedWrite = o;
         emit delayedWriteChanged();
+        
         if (m_delayedWrite) {
+            // Starting a write operation
+            if (m_image) {
+                QString reason = tr("Writing %1 to flash drive").arg(m_image->fullName());
+                SuspendInhibitor::inhibit(reason);
+            }
             write(m_image);
         } else {
+            // Write operation finished
             cancel();
+            
+            // Check if we should release suspend inhibition
+            // Only release if no downloads are active and no other drives are writing
+            bool hasOngoingOperations = false;
+            
+            // Check downloads
+            DriveManager *driveManager = DriveManager::instance();
+            if (driveManager) {
+                // Note: We would need access to ReleaseManager to check download status
+                // For simplicity, we'll let the ReleaseVariant::setStatus handle this
+            }
+            
+            // Check other drives
+            for (int i = 0; i < driveManager->length(); i++) {
+                Drive *drive = driveManager->get(i);
+                if (drive && drive != this && drive->delayedWrite()) {
+                    hasOngoingOperations = true;
+                    break;
+                }
+            }
+            
+            if (!hasOngoingOperations) {
+                SuspendInhibitor::release();
+            }
         }
     }
 }
@@ -311,24 +344,26 @@ void Drive::setImage(ReleaseVariant *data)
 
 bool Drive::write(ReleaseVariant *data)
 {
-    m_image = data;
-    m_image->setErrorString(QString());
-    if (data && data->size() > 0 && size() > 0 && data->realSize() > size()) {
-        m_image->setErrorString(tr("This drive is not large enough."));
-        setDelayedWrite(false);
+    if (!data)
         return false;
-    }
 
+    m_image = data;
+    
+    // Register write operation for suspend inhibition
+    QString operationId = QString("write_%1").arg(reinterpret_cast<quintptr>(this));
+    QString reason = tr("Writing %1 to flash drive").arg(data->fullName());
+    SuspendManager::instance()->registerOperation(operationId, reason);
+    
     return true;
 }
 
 void Drive::cancel()
 {
-    m_delayedWrite = false;
-    emit delayedWriteChanged();
-    m_error = QString();
-    m_restoreStatus = CLEAN;
-    emit restoreStatusChanged();
+    // Unregister write operation
+    QString operationId = QString("write_%1").arg(reinterpret_cast<quintptr>(this));
+    SuspendManager::instance()->unregisterOperation(operationId);
+    
+    m_image = nullptr;
 }
 
 bool Drive::operator==(const Drive &o) const
--- src/app/releasemanager.cpp
+++ src/app/releasemanager.cpp
@@ -27,6 +27,8 @@
 #include <QtQml>
 
 #include <QJsonDocument>
+#include "suspendinhibitor.h"
+#include "suspendmanager.h"
 
 using namespace Qt::Literals::StringLiterals;
 
@@ -1077,7 +1079,42 @@ bool ReleaseVariant::erase()
 void ReleaseVariant::setStatus(Status s)
 {
     if (m_status != s) {
+        Status oldStatus = m_status;
         m_status = s;
+        
+        // Handle suspend inhibition based on status changes
+        bool shouldInhibit = (s == DOWNLOADING || s == DOWNLOAD_VERIFYING || s == WRITING || s == WRITE_VERIFYING);
+        bool wasInhibiting = (oldStatus == DOWNLOADING || oldStatus == DOWNLOAD_VERIFYING || oldStatus == WRITING || oldStatus == WRITE_VERIFYING);
+        
+        if (shouldInhibit && !wasInhibiting) {
+            QString reason;
+            if (s == DOWNLOADING || s == DOWNLOAD_VERIFYING) {
+                reason = tr("Downloading %1").arg(fullName());
+            } else {
+                reason = tr("Writing %1 to flash drive").arg(fullName());
+            }
+            SuspendManager::instance()->registerOperation(QString("download_%1").arg(reinterpret_cast<quintptr>(this)), reason);
+        } else if (!shouldInhibit && wasInhibiting) {
+            // Check if there are any other ongoing operations before releasing
+            // This is important because multiple operations might be running
+            bool hasOngoingOperations = false;
+            
+            // Check if any drive is currently writing
+            DriveManager *driveManager = DriveManager::instance();
+            for (int i = 0; i < driveManager->length(); i++) {
+                Drive *drive = driveManager->get(i);
+                if (drive && drive->delayedWrite()) {
+                    hasOngoingOperations = true;
+                    break;
+                }
+            }
+            
+            // If no ongoing operations, release inhibition
+            if (!hasOngoingOperations) {
+                SuspendManager::instance()->unregisterOperation(QString("download_%1").arg(reinterpret_cast<quintptr>(this)));
+            }
+        }
+        
         emit statusChanged();
     }
 }
